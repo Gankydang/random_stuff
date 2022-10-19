a81c99303a9b0b39a5be15b88e62bdd7abe55743
Gankydang <108336379+Gankydang@users.noreply.github.com>
2022-10-19 23:30:19 +0800

    Update tictactoetomek.py

--- tictactoetomek.py
+++ tictactoetomek.py
@@ -1,38 +1,85 @@
 import random
 
-board = []
 
-for y in range(4):
-    board.append([])
-    for x in range(4):
-        board[y].append('.')
-
-#board[random.randint(0, 3)][random.randint(0, 3)] = 'T'
-
-def displayBoard(board):
+def makeBoard():
+    bo = []
     for y in range(4):
+        bo.append([])
         for x in range(4):
-            print(board[x][y], end='')
+            bo[y].append('.')
+    bo[random.randint(0, 3)][random.randint(0, 3)] = 'T'
+    return bo
+
+def displayBoard(bo):
+    print('  0 1 2 3')
+    for y in range(4):
+        print(y, end=' ')
+        for x in range(4):
+            print(bo[x][y], end=' ')
         print()
 
-def getPlayerMove(board, sym):
+def getPlayerMove(bo, turn):
     try:
-        move = input('Enter your move: ').split(' ')
+        move = input(f'Player {turn} enter your move: ').strip().split(' ')
         move = list(map(lambda x: int(x), move))
         x, y = move
         
-        if x not in range(4) or y not in range(4):
-            return getPlayerMove(board, sym)
-        elif board[x][y] in ['X', 'O', 'T']:
-            return getPlayerMove(board, sym)
+        if x not in range(4) or y not in range(4) or bo[x][y] != '.':
+            print('Space taken.')
+            return getPlayerMove(bo, turn)
     except:
         print('Invalid move.')
-        return getPlayerMove(board, sym)
+        return getPlayerMove(bo, turn)
     else:
-        return [sym, move]
+        return move
 
-x = getPlayerMove(board, 'X')
-print(x)
-            
+def checkWin(bo, sym):
+    chars = [sym, 'T']
+    return ((bo[0][0] in chars and bo[1][0] in chars and bo[2][0] in chars and bo[3][0] in chars) or
+    (bo[0][1] in chars and bo[1][1] in chars and bo[2][1] in chars and bo[3][1] in chars) or
+    (bo[0][2] in chars and bo[1][2] in chars and bo[2][2] in chars and bo[3][2] in chars) or
+    (bo[0][3] in chars and bo[1][3] in chars and bo[2][3] in chars and bo[3][3] in chars) or
 
+    (bo[0][0] in chars and bo[0][1] in chars and bo[0][2] in chars and bo[0][3] in chars) or
+    (bo[1][0] in chars and bo[1][1] in chars and bo[1][2] in chars and bo[1][3] in chars) or
+    (bo[2][0] in chars and bo[2][1] in chars and bo[2][2] in chars and bo[2][3] in chars) or
+    (bo[3][0] in chars and bo[3][1] in chars and bo[3][2] in chars and bo[3][3] in chars) or
 
+    (bo[0][0] in chars and bo[1][1] in chars and bo[2][2] in chars and bo[3][3] in chars) or
+    (bo[3][0] in chars and bo[2][1] in chars and bo[1][2] in chars and bo[0][3] in chars)
+    )
+
+def checkDraw(bo):
+    for y in range(4):
+        for x in range(4):
+            if bo[x][y] == '.':
+                return False
+    return True
+
+def main():
+    board = makeBoard()
+    turnx = True
+    displayBoard(board)
+    print()
+
+    while True:
+        if turnx:
+            turn = 'X'
+        else:
+            turn = 'O'
+
+        print()
+        x, y = getPlayerMove(board, turn)
+        board[x][y] = turn
+        print()
+        displayBoard(board)
+
+        if checkWin(board, turn):
+            print(f'{turn} won the game!')
+            break
+        elif checkDraw(board):
+            print('The game is a draw!')
+            break
+        turnx = not turnx
+    
+main()
