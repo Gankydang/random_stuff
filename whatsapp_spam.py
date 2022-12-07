99520ffa90d628378553efb98a69575c2d414adc
Gankydang <108336379+Gankydang@users.noreply.github.com>
2022-12-08 00:05:55 +0800

    Update whatsapp_spam.py

--- whatsapp_spam.py
+++ whatsapp_spam.py
@@ -1,11 +1,8 @@
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from time import sleep
-
-chat_name = 'SuPeR' # CHANGE ACCORDING TO THE CHAT NAME YOU WANT TO SPAM
-num_of_msg = 200 # CHANGE ACCORDING TO THE NUMBER OF MESSAGES YOU WANT TO SEND
-msg = 'hi' # CHANGE ACCORDING TO THE MESSAGE YOU WANT TO SPAM
 
 def find_elem(by, xp):
     found = False
@@ -21,8 +18,9 @@ def find_elem(by, xp):
 driver = webdriver.Chrome()
 driver.get('https://web.whatsapp.com/')
 
+chat_name = 'Sanjeev' # CHANGE ACCORDING TO THE CHAT NAME YOU WANT TO SPAM
 
-chat_image_xp = f'//*[contains(text(), {chat_name})]'
+chat_image_xp = f'//*[contains(text(), "{chat_name}")]'
 chat_image_elem = find_elem(By.XPATH, chat_image_xp)
 sleep(3)
 chat_image_elem.click()
@@ -30,6 +28,8 @@ chat_image_elem.click()
 text_box_xp = '//*[@id="main"]/footer/div[1]/div/span[2]/div/div[2]/div[1]'
 text_box_elem = find_elem(By.XPATH, text_box_xp)
 
+num_of_msg = 50 # CHANGE ACCORDING TO THE NUMBER OF MESSAGES YOU WANT TO SEND
+msg = 'hi' # CHANGE ACCORDING TO THE MESSAGE YOU WANT TO SPAM
 for i in range(num_of_msg):
     text_box_elem.send_keys(msg, Keys.ENTER)
 
