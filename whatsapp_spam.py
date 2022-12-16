3f7b981ce25d33817131ac124d3d67a3b2db1d61
Gankydang <108336379+Gankydang@users.noreply.github.com>
2022-12-17 00:04:11 +0800

    Update whatsapp_spam.py

--- whatsapp_spam.py
+++ whatsapp_spam.py
@@ -1,4 +1,3 @@
-
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
@@ -18,7 +17,7 @@ def find_elem(by, xp):
 driver = webdriver.Chrome()
 driver.get('https://web.whatsapp.com/')
 
-chat_name = 'Sanjeev' # CHANGE ACCORDING TO THE CHAT NAME YOU WANT TO SPAM
+chat_name = 'JY' # CHANGE ACCORDING TO THE CHAT NAME YOU WANT TO SPAM
 
 chat_image_xp = f'//*[contains(text(), "{chat_name}")]'
 chat_image_elem = find_elem(By.XPATH, chat_image_xp)
@@ -28,9 +27,9 @@ chat_image_elem.click()
 text_box_xp = '//*[@id="main"]/footer/div[1]/div/span[2]/div/div[2]/div[1]'
 text_box_elem = find_elem(By.XPATH, text_box_xp)
 
-num_of_msg = 50 # CHANGE ACCORDING TO THE NUMBER OF MESSAGES YOU WANT TO SEND
+num_of_msg = 2 # CHANGE ACCORDING TO THE NUMBER OF MESSAGES YOU WANT TO SEND
 msg = 'hi' # CHANGE ACCORDING TO THE MESSAGE YOU WANT TO SPAM
 for i in range(num_of_msg):
     text_box_elem.send_keys(msg, Keys.ENTER)
 
-sleep(100)
+input('Press enter when done...')
