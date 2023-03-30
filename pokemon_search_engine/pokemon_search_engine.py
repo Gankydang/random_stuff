e0fd9cd3d574dd62ed244cfb71ec27889bbcef14
Brian <brian@Brians-MacBook-Air.local>
2023-03-30 12:59:47 +0800

    changed pokemon search engine

--- pokemon_search_engine/pokemon_search_engine.py
+++ pokemon_search_engine/pokemon_search_engine.py
@@ -1,57 +1,14 @@
 from sys import exit
-import csv
+import pandas as pd
 import os
+from numpy import nan
+import matplotlib.pyplot as plt
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
-HEADERS = 'No  Name                          Type 1    Type 2    Total  HP     Attack    Defense   Sp. Atk   Sp. Def   Speed     Generation   Legendary'
-
-def read_csv():
-    '''
-    Reads Pokemon.csv and stores the information in a global variable all_pokemon_info:
-    [
-        {'No': 1, 'Name': 'Bulbasaur', 'Type 1': 'Grass', 'Type 2': 'Poison', 'Total': 318, 
-        'HP': 45, 'Attack': 49, 'Defense': 49, 'Sp. Atk': 65, 'Sp. Def': 65, 'Speed': 45, 
-        'Generation': 1, 'Legendary': 'FALSE'}, ....
-    ]
-    '''
-
-    global all_pokemon_info
-    with open(os.path.join(ROOT_DIR, 'Pokemon.csv')) as f:
-        # stores information from csv into ls
-        csv_reader = csv.reader(f)
-        ls = []
-        for row in csv_reader:
-            row = list(map(lambda x: int(x) if x.isdigit() else x, row))
-            ls.append(row) 
-
-        # converts ls into a usable format which is stored in all_pokemon_info
-        all_pokemon_info = []
-        for i in range(1, len(ls)):
-            row = ls[i]
-            each_pokemon_info = {}
-            start_from = 0
-
-            for stat in row:
-                index = row.index(stat, start_from)
-                start_from = index + 1
-                each_pokemon_info[ls[0][index]] = row[index]
-            all_pokemon_info.append(each_pokemon_info)
-
-def get_printable_row(row: dict):
-    '''
-    Accepts the row as a dictionary and returns the printable string of it
-    '''
-
-    row = list(row.values())
-    len_of_portion = [4, 30, 10, 10, 7, 7, 10, 10, 10, 10, 10, 13, 9]
-    row_to_print = ''
-    for stat, length in zip(row, len_of_portion):
-        row_to_print += str(stat).ljust(length)
-    return row_to_print + '\n'
 
 def get_option():
     '''
-    Displays options and asks user to choose an option from 0 to 5.
+    Displays options and asks user to choose an option from 0 to 6.
     Returns the chosen option
     '''
 
@@ -62,90 +19,75 @@ def get_option():
     print('3. Display all Pokemons with Total Base stat of your choice')
     print('4. Display all Pokemons with a minimum set of stats')
     print('5. Display all legendary Pokemons of specific Type1 and Type2')
+    print('6. Find out the best Type of pokemon')
     print('0. Quit\n')
 
     option = input('Enter option: ').strip()
     print()
-    if option not in '0 1 2 3 4 5'.split():
+    # ensure option is either 0, 1, 2, 3, 4, 5, or 6
+    if option not in '0 1 2 3 4 5 6'.split():
         print('Not implemented yet? Please choose a valid option.\n')
         return get_option()
     option = int(option)
     if option == 0:
+        # exit the program
         exit('Thank you for using the Pokemon Super Search Engine!')
     return option
 
 def option_1():
     '''
-    Display selected number of Pokemons with their types and statistics
+    Returns the selected number of Pokemons with their types and statistics
     '''
 
     # get number of pokemons to display
     num_of_pokemons = ''
-    len_all_pokemon_info = len(all_pokemon_info)
+    len_all_pokemon_info = len(pokemon_df)
     while (not num_of_pokemons.isdigit()) or not (int(num_of_pokemons) > 0 and int(num_of_pokemons) < len_all_pokemon_info):
         num_of_pokemons = input('Enter number of Pokemons to be displayed: ').strip()
         print()
     num_of_pokemons = int(num_of_pokemons)
     
-    # display pokemons
-    table = ''
-    print(HEADERS)
-    for row in all_pokemon_info[: num_of_pokemons]:
-        table += get_printable_row(row)
-    print(table)
+    return pokemon_df.loc[pokemon_df['No'] <= num_of_pokemons]
 
 def option_2():
     '''
-    Display the first Pokemon of a Type of the trainder's choice
+    Returns the first Pokemon of a Type of the trainder's choice
     '''
-
-    # get all possible types of pokemon
-    list_of_all_types = []
-    for row in all_pokemon_info:
-        for i in range(1, 3):
-            if row[f'Type {i}'] != '' and row[f'Type {i}'] not in list_of_all_types:
-                list_of_all_types.append(row[f'Type {i}'])
 
     print(f'Available types: {", ".join(list_of_all_types)}')
     type = input('Enter Type: ').strip().capitalize()
-    pokemon_found = False
-
-    # if type exists, print the first pokemon of that type
-    if type in list_of_all_types:
-        print(HEADERS)
-        for row in all_pokemon_info:
-            for i in range(1, 3):
-                if row[f'Type {i}'] == type: 
-                    pokemon_found = True
-                    print(get_printable_row(row))
-            if pokemon_found:
-                break
+    print()
+    
+    # if type exists, return the first pokemon of that type
+    if not type in list_of_all_types:
+        print('No pokemon of this type.')
     else:
-        print('No pokemon of this type.')    
+        for row in pokemon_df.iterrows():
+            if row[1]['Type 1'] == type or row[1]['Type 2'] == type:
+                return pokemon_df.loc[[row[1]['No'] - 1]]
+  
 
 def option_3():
     '''
-    Display all Pokemons with Total Base stat of the trainer's choice
+    Returns all Pokemons with Total Base stat of the trainer's choice
     '''
 
     try:
+        # get total base stat
         total_base_stat = int(input('Enter Total Base stat: ').strip())
-        to_print = ''
-        for row in all_pokemon_info:
-            if row['Total'] == total_base_stat:
-                to_print += get_printable_row(row)
+        print()
+        to_print = pokemon_df.loc[pokemon_df['Total'] == total_base_stat]
 
         # if no pokemon with the specified total base stat, raise error
-        if to_print == '':
+        if to_print.empty:
             raise Exception()
-        print(HEADERS)
-        print(to_print)
+        return to_print
     except:
         print('No pokemon with this Total Base stat.')
 
 def option_4():
     '''
-    Display all Pokemons with a minimum set of stats
+    Returns all Pokemons with a minimum set of stats
     '''
 
     # get minimum set of stats from user
@@ -153,6 +95,7 @@ def option_4():
     min_stats['Sp. Atk'] = input('Enter min special attack stat: ').strip()
     min_stats['Sp. Def'] = input('Enter min special defense stat: ').strip()
     min_stats['Speed'] = input('Enter min speed stat: ').strip()
+
     for i in min_stats.values():
         if not i.isdigit():
             print('Enter a number.\n')
@@ -162,49 +105,93 @@ def option_4():
 
         # get highest possible stats of all pokemon
         highest_stats = {'Sp. Atk': 0, 'Sp. Def': 0, 'Speed': 0}
-        for row in all_pokemon_info:
-            if row['Sp. Atk'] > highest_stats['Sp. Atk']:
-                highest_stats['Sp. Atk'] = row['Sp. Atk']
-            if row['Sp. Def'] > highest_stats['Sp. Def']:
-                highest_stats['Sp. Def'] = row['Sp. Def']
-            if row['Speed'] > highest_stats['Speed']:
-                highest_stats['Speed'] = row['Speed']
+        highest_stats['Sp. Atk'] = pokemon_df['Sp. Atk'].max()
+        highest_stats['Sp. Def'] = pokemon_df['Sp. Def'].max()
+        highest_stats['Speed'] = pokemon_df['Speed'].max()
 
-        to_print = ''
-        for row in all_pokemon_info:
-            if highest_stats['Sp. Atk'] < min_stats['Sp. Atk'] or highest_stats['Sp. Def'] < min_stats['Sp. Def'] or highest_stats['Speed'] < min_stats['Speed']:
-                print('No pokmeon has such powerful stats.\n')
-                break
-            else:
-                if row['Sp. Atk'] >= min_stats['Sp. Atk'] and row['Sp. Def'] >= min_stats['Sp. Def'] and row['Speed'] >= min_stats['Speed']:
-                    to_print += get_printable_row(row)
+        # check whether the minimum stats entered by the user is greater than the highest possible stats
+        if highest_stats['Sp. Atk'] < min_stats['Sp. Atk'] or highest_stats['Sp. Def'] < min_stats['Sp. Def'] or highest_stats['Speed'] < min_stats['Speed']:
+            print('No pokmeon has such powerful stats.\n')
         else:
-            print(HEADERS)
-            print(to_print)
+            return pokemon_df.loc[(pokemon_df['Sp. Atk'] >= min_stats['Sp. Atk']) & (pokemon_df['Sp. Def'] >= min_stats['Sp. Def']) & (pokemon_df['Speed'] >= min_stats['Speed'])]
 
 def option_5():
     '''
-    Display all legendary Pokemons of Types of the trainer's choice
+    Returns all legendary Pokemons of Types of the trainer's choice
     '''
 
-    type1 = input('Enter Type1: ').strip().capitalize()
-    type2 = input('Enter Type2: ').strip().capitalize()
+    # get type 1 and type 2
+    print(f'Available types: {", ".join(list_of_all_types)}')
+    type1 = input('Enter Type 1: ').strip().capitalize()
+    type2 = input('Enter Type 2: ').strip().capitalize()
+    to_print = pokemon_df.loc[(pokemon_df['Legendary'] == True) & (pokemon_df['Type 1'] == type1) & (pokemon_df['Type 2'] == type2)]
 
-    to_print = ''
-    for row in all_pokemon_info:
-        if row['Legendary'] == 'TRUE' and row['Type 1'] == type1 and row['Type 2'] == type2:
-            to_print += get_printable_row(row)
-    if to_print == '':
-        print('No such legendary Pokemon.')
+    if to_print.empty:
+        print('No such legenary Pokemon')
     else:
-        print(HEADERS)
-        print(to_print)
+        return to_print
+    
+def option_6():
+    '''
+    Plots bar chart of the mean Total Stats of each Type of Pokemon
+    '''
+
+    # populates the dictionary with - type: mean total stats of all of the pokemon of that type
+    type_to_total_mapping = {}
+    for t in list_of_all_types:
+        type_to_total_mapping[t] = 0
+        num_of_pokemon = 0
+        for total in pokemon_df.loc[(pokemon_df['Type 1'] == t) | (pokemon_df['Type 2'] == t)]['Total']:
+            type_to_total_mapping[t] += total
+            num_of_pokemon += 1
+        type_to_total_mapping[t] = type_to_total_mapping[t] / num_of_pokemon
+    
+    reversed_type_to_total_mapping = {round(value): key for key, value in type_to_total_mapping.items()}
+    sorted_types = []
+    sorted_values = []
+    for val in sorted(list(reversed_type_to_total_mapping.keys()), reverse=True):
+        sorted_types.append(reversed_type_to_total_mapping[val])
+        sorted_values.append(val)     
+    
+    # plot bar chart
+    plt.bar(x=sorted_types, height=sorted_values, color='green')
+    plt.title('Mean Total Stats of each Type of Pokemon')
+    plt.xlabel('Type of Pokemon')
+    plt.ylabel('Mean Total Stats')
+    plt.ylim(350)
+
+    # displays the values on top of each bar
+    for i in range(len(sorted_types)):
+        plt.text(i, sorted_values[i], sorted_values[i], ha='center', color='blue', fontweight='bold')
+
+    print('Expand the graph to full screen.')
+    print(f'As we can see, {sorted_types[0]} is the strongest type.')
+    print('To continue, close the graph window.')
+    plt.show()
 
 def main():
-    read_csv()
+
+    # read Pokemon.csv into a data frame
+    global pokemon_df
+    pokemon_df = pd.read_csv(os.path.join(ROOT_DIR, 'Pokemon.csv')).replace(nan, '')
+
+    # get all possible types of pokemon
+    global list_of_all_types
+    list_of_all_types = []
+    for type1 in pokemon_df['Type 1']:
+        if type1 != '' and type1 not in list_of_all_types:
+            list_of_all_types.append(type1)
+    for type2 in pokemon_df['Type 2']:
+        if type2 != '' and type2 not in list_of_all_types:
+            list_of_all_types.append(type2)
+
     option = get_option()
-    options = {1: option_1, 2: option_2, 3: option_3, 4: option_4, 5: option_5}
-    options[option]()
+    options = {1: option_1, 2: option_2, 3: option_3, 4: option_4, 5: option_5, 6: option_6}
+    to_print = options[option]()
+
+    if to_print is not None: # check whether to_print is empty
+        print(to_print.to_string(index=False))
+
     return main()
 
 if __name__ == '__main__':
